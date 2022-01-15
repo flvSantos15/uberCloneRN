9efc7c2511ed44c8e144c2c2bb4a2dd688aba40a
Flavio Santos <flvsantos300@gmail.com>
2022-01-14 22:56:39 -0300

    a few icons added

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
 
 const data = [
   {
@@ -47,6 +48,11 @@ const NavOptions = () => {
             <Text style={tw`mt-2 text-lg font-semibold`}>
               {item.title}
             </Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright' 
+              color='white' 
+              type='antdesign'/>
           </View>
         </TouchableOpacity>
       )}
