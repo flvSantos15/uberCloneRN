94075efaa67f81df1e2229251362982cba13a224
Flavio Santos <flvsantos300@gmail.com>
2022-01-15 23:56:05 -0300

    added navigation

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 const data = [
   {
@@ -19,6 +20,8 @@ const data = [
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation()
+
   return (
     <FlatList
       data={data}
@@ -26,15 +29,8 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       renderItem={({item}) => (
         <TouchableOpacity
-          style={tw`
-            p-2 
-            pl-6 
-            pb-8 
-            pt-4 
-            bg-gray-200
-            m-2
-            w-40
-          `}
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
         >
           <View>
             <Image
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
 
 const MapScreen = () => {
   return (
-    <View>
+    <View style={tw`bg-white h-full p-5`}>
       <Text>Here is the map stuff...</Text>
     </View>
   )
